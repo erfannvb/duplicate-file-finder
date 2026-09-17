013cb22b3c27b3580216e49a840ec59d9f553b8c
erfannvb <erfannavab@gmail.com>
2026-09-17 22:31:57 +0330

    feat: filter potential duplicate groups

--- src/main/java/nvb/dev/duplicatefilefinder/FileSizeGrouper.java
+++ src/main/java/nvb/dev/duplicatefilefinder/FileSizeGrouper.java
@@ -12,12 +12,20 @@ public class FileSizeGrouper {
 
     public static Map<Long, List<Path>> group(List<Path> files) throws IOException {
         Map<Long, List<Path>> result = new HashMap<>();
-
         for (Path file : files) {
             long fileSize = Files.size(file);
             result.computeIfAbsent(fileSize, k -> new ArrayList<>()).add(file);
         }
+        return result;
+    }
 
+    public static Map<Long, List<Path>> filterPotentialDuplicates(Map<Long, List<Path>> groupedFiles) {
+        Map<Long, List<Path>> result = new HashMap<>();
+        for (Map.Entry<Long, List<Path>> entry : groupedFiles.entrySet()) {
+            List<Path> fileList = entry.getValue();
+            if (fileList.size() > 1)
+                result.put(entry.getKey(), fileList);
+        }
         return result;
     }
 
