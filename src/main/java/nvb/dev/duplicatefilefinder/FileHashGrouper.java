b19dc07650c10c4b1c65b3804ce95275c0ece340
erfannvb <erfannavab@gmail.com>
2026-09-18 12:55:46 +0330

    feat: duplicate file groups

--- src/main/java/nvb/dev/duplicatefilefinder/FileHashGrouper.java
+++ src/main/java/nvb/dev/duplicatefilefinder/FileHashGrouper.java
@@ -1,6 +1,7 @@
 package nvb.dev.duplicatefilefinder;
 
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,6 +12,15 @@ public class FileHashGrouper {
         return files
                 .stream()
                 .collect(Collectors.groupingBy(FileHasher::hash));
+    }
+
+    public static Map<String, List<Path>> filterDuplicateGroups(Map<String, List<Path>> groupedFiles) {
+        Map<String, List<Path>> filteredMap = new HashMap<>();
+        for (Map.Entry<String, List<Path>> entry : groupedFiles.entrySet()) {
+            if (entry.getValue().size() > 1)
+                filteredMap.put(entry.getKey(), entry.getValue());
+        }
+        return filteredMap;
     }
 
 }
